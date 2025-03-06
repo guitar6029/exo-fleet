c16ee525bb8ce7bab0e0e42c748c3c3ffe7703fa
Josh <jsbusiness305@gmail.com>
2025-03-06 17:24:11 -0500

    removed unused icons, added bg imge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="min-h-screen">{children}</main>
       </body>
     </html>
   );
