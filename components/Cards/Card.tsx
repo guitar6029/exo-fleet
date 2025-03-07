e042b9de46d389694d4890dd51165fcaecab9b76
Josh <jsbusiness305@gmail.com>
2025-03-06 22:04:43 -0500

    updated Card component

---
+++ components/Cards/Card.tsx
@@ -0,0 +1,49 @@
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+
+type DirectionType = "left" | "right";
+
+type CardProps = {
+  title?: string;
+  description: string;
+  imageSrc: StaticImageData;
+  direction?: DirectionType;
+  linkSrc?: string;
+};
+
+export default function Card({
+  title,
+  description,
+  imageSrc,
+  direction = "left",
+  linkSrc,
+}: CardProps) {
+  const cardInfo =
+    direction === "left" ? (
+      <div className="grid grid-cols-2 rounded-full p-4 ">
+        <div className="col-span-1">
+          <Image src={imageSrc} alt={title ? title : "Card Image" } width={500} height={500} className="w-full" />
+        </div>
+
+        <div className="col-span-1 flex flex-col items-center justify-center gap-2  bg-neutral-200 p-2 bg-gradient-to-br from-slate-100 to-slate-700 ">
+          <h1 className="neon-vortex text-2xl font-bold">{title}</h1>
+          <p className="font-mono text-center">{description}</p>
+          {linkSrc && <Link href={linkSrc}></Link>}
+        </div>
+      </div>
+    ) : (
+      <div className="grid grid-cols-2 rounded-full p-4">
+        <div className="col-span-1 flex flex-col items-center justify-center gap-2  bg-neutral-200 p-2 bg-gradient-to-br from-slate-100 to-slate-700 ">
+          <h1 className="neon-vortex text-2xl font-bold">{title}</h1>
+          <p className="font-mono text-center">{description}</p>
+          {linkSrc && <Link href={linkSrc}></Link>}
+        </div>
+
+        <div className="col-span-1">
+          <Image src={imageSrc} alt={title ? title : "Card Image"} width={500} height={500} className="w-full" />
+        </div>
+      </div>
+    );
+
+  return cardInfo;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
 import Hero from "../../components/Hero/Hero";
+import Card from "../../components/Cards/Card";
+import SpaceStationImage from "@/assets/images/space-station-2.jpg";
+import SpaceStation3 from "@/assets/images/space-station-3.jpg";
+import { StaticImageData } from "next/image";
+
+type TCard = {
+  title?: string;
+  description: string;
+  img: StaticImageData;
+};
+
+const cards: TCard[] = [
+  {
+    title: "What is ExoFleet?",
+    description:
+      "ExoFleet is a cutting-edge space station simulation where you can command fleets of ships, build stations, and control entire space operations. Set your course, explore new planets, and manage resources to grow your fleet. The universe is waiting.",
+    img: SpaceStationImage,
+  },
+  {
+    title: "Features",
+    description:
+      "ExoFleet offers a rich set of features to keep you engaged. From managing your fleet of ships to mining precious resources, your journey is filled with strategic decision-making. Build stations, explore new worlds, and keep your fleet in top shape for whatever challenges lie ahead.",
+    img: SpaceStation3,
+  },
+];
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      {cards.map((card: TCard, index: number) => {
+        return (
+          <div key={index} className="min-h-screen flex flex-col items-center justify-center">
+            <Card
+              title={card.title}
+              description={card.description}
+              imageSrc={card.img}
+              direction={index % 2 === 0 ? "left" : "right"}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
