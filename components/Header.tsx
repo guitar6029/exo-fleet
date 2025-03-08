d7426ee8f99083d744d6cabaf74b3697aa1b4bcd
Josh <jsbusiness305@gmail.com>
2025-03-08 11:08:06 -0500

    added arrivals / departures links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link"
+
 export default function Header() {
     return (
-        <div className="flex flex-row items-center p-3 bg-gradient-to-br from-blue-200 to-blue-300 text-black">
-            <h1 className="neon-vortex text-2xl font-bold">ExoFleet</h1>
+        <div className="flex flex-row items-center gap-4 p-3 bg-gradient-to-br from-blue-200 to-blue-300 text-black">
+            <Link href={"/"}><h1 className="neon-vortex text-2xl font-bold">ExoFleet</h1></Link>
+            <Link href={"/flights/arrivals"}><h1 className="text-xl font-bold font-mono uppercase">Arrivals</h1></Link>
+            <Link href={"/flights/departures"}><h1 className="text-xl font-bold font-mono uppercase">Departures</h1></Link>
         </div>
     )
 }
